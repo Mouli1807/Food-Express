ee77d9b2ef98d9beccb6a988adf0d89821183074
Mouli1807 <74283228+Mouli1807@users.noreply.github.com>
2025-04-24 22:28:44 +0530

    updated menuapi

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const CART_URL =
   "https://www.svgrepo.com/show/80543/shopping-cart-outline.svg";
-export const MENU_API = "http://localhost:5050/api/menu/";
-export const RESTAURANT_API = "http://localhost:5050/api/restaurants";
+export const MENU_API = "https://food-express-eight.vercel.app/api/menu";
+export const RESTAURANT_API = "https://food-express-eight.vercel.app/api/restaurants";
